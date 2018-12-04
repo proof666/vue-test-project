edb5e9454f71a85e167ee8d210d0e84f64c6b845
proof666 <proof666@MacBook-Pushkin.local>
2018-12-05 02:42:03 +0300

    test: setup test environment for Jest

--- client/test/unit/setup.js
+++ client/test/unit/setup.js
@@ -1,3 +1,14 @@
 import Vue from 'vue';
+import io from 'socket.io-client';
 
 Vue.config.productionTip = false;
+Vue.prototype.$io = io('localhost:8081', {
+  'sync disconnect on unload': true,
+});
+
+const fetchPolifill = require('whatwg-fetch');
+
+global.fetch = fetchPolifill.fetch;
+global.Request = fetchPolifill.Request;
+global.Headers = fetchPolifill.Headers;
+global.Response = fetchPolifill.Response;
