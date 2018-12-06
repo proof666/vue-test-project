048e3d7298d7031fedf7344ea4e33971456526c5
Sergey Pushkin <Pushkin@avp.ru>
2018-12-06 20:10:09 +0300

    fix: fix situation when client recieve duplicating data and fix table sorting

--- client/src/views/Data.vue
+++ client/src/views/Data.vue
@@ -2,7 +2,7 @@
 <div>
 <H1>Data</H1>
   <div class='container'>
-    <data-table v-bind:loading="inProgress" v-bind:headers="headers" v-bind:data="dataStore.data"></data-table>
+    <data-table v-bind:loading="inProgress" v-bind:headers="headers" v-bind:data="sortedData"></data-table>
   </div>
 </div>
 </template>
@@ -30,11 +30,22 @@ export default {
       inProgress: true,
     };
   },
-  created() {
-    fetch(`${process.env.BACKEND_HOST}/data`).then(response => response.json()).then((data) => { this.dataStore = data; }).finally(()=> {this.inProgress = false});
+  computed: {
+    sortedData() {
+      return this.dataStore.data ? this.dataStore.data.sort(function(a, b){
+        if(a.datetime > b.datetime) { return -1; }
+        if(a.datetime < b.datetime) { return 1; }
+        return 0;
+      }) : null
+    }
+  },
+  mounted() {
     this.$io.on('addData', (data) => {
       this.dataStore.data.push(data);
     });
+  },
+  created() {
+    fetch(`${process.env.BACKEND_HOST}/data`).then(response => response.json()).then((data) => { this.dataStore = data; }).finally(()=> {this.inProgress = false});
   },
 };
 </script>
--- client/src/views/Logs.vue
+++ client/src/views/Logs.vue
@@ -2,7 +2,7 @@
 <div>
 <H1>Logs</H1>
   <div class='container'>
-    <data-table v-bind:loading="inProgress" v-bind:headers="headers" v-bind:data="log.data"></data-table>
+    <data-table v-bind:loading="inProgress" v-bind:headers="headers" v-bind:data="sortedData"></data-table>
   </div>
 </div>
 </template>
@@ -30,14 +30,28 @@ export default {
       inProgress: true,
     };
   },
-  created() {
-    fetch(`${process.env.BACKEND_HOST}/logs`).then(response => response.json()).then((log) => { this.log = log; }).finally(()=> {this.inProgress = false});
+  computed: {
+    sortedData() {
+      return this.log.data ? this.log.data.sort(function(a, b){
+        if(a.datetime > b.datetime) { return -1; }
+        if(a.datetime < b.datetime) { return 1; }
+        return 0;
+      }) : null
+    }
+  },
+  mounted() {
     this.$io.on('login', (data) => {
       this.log.data.push(data);
     });
     this.$io.on('logout', (data) => {
       this.log.data.push(data);
     });
+    this.$io.on('addData', (data) => {
+      this.log.data.push(data);
+    });
+  },
+  created() {
+    fetch(`${process.env.BACKEND_HOST}/logs`).then(response => response.json()).then((log) => { this.log = log; }).finally(()=> {this.inProgress = false});
   },
 };
 </script>
